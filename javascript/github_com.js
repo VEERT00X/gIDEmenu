f5f596314b55d8488b2249def31518f7e0fb0eb3
VEERT00X <github@veert00x.com>
2022-08-18 19:04:34 +0200

    l

--- javascript/github_com.js
+++ javascript/github_com.js
@@ -1,6 +1,12 @@
 const platform = location.host.split('.')[0];
 console.log("Github.com extension loaded, platform: " + platform);
 
+function GetUsername() {
+    var UserName = document.querySelector("body > div.position-relative.js-header-wrapper > header > div.Header-item.position-relative.mr-0.d-none.d-md-flex > details > summary > img").alt;
+    var UserName = UserName.replace("@", "");
+    return UserName;    
+}
+
 async function init () {
   
   switch (platform) {
@@ -19,7 +25,7 @@ async function init () {
         Ide_div.className = "dropdown-menu dropdown-menu-sw";
         IDE_Details.appendChild(Ide_div);
 
-        chrome.storage.sync.get(["checkbox1", "checkbox2", "checkbox3", "checkbox5", "checkbox6", "checkbox7"], function(result) {
+        chrome.storage.sync.get(["checkbox1", "checkbox2", "checkbox3", "checkbox4", "checkbox5", "checkbox6", "checkbox7"], function(result) {
             const checkbox1 = result.checkbox1;
             const checkbox2 = result.checkbox2;
             const checkbox3 = result.checkbox3;
@@ -28,12 +34,28 @@ async function init () {
             const checkbox6 = result.checkbox6;
             console.log(checkbox1 , checkbox2 , checkbox3 , checkbox4 , checkbox5 , checkbox6);
             
+            if (checkbox4 == true) {
+                console.log("checkbox4 is true");
+                var VisualStudio_btn = document.createElement("button");
+                VisualStudio_btn.className = "dropdown-item btn-link sas open-in-vscode-local";
+                VisualStudio_btn.innerHTML = "Open in Visual Studio";
+                VisualStudio_btn.onclick = function() {
+                    const VS_url = "git-client://clone?repo=https%3A%2F%2Fgithub.com%2F";
+                    const UserName = GetUsername();
+                    const RepoName = MainUrl.split("/")[4];
+                    const VS_url_full = VS_url + UserName + "%2F" + RepoName;
+                    window.location.href = VS_url_full;
+                }
+                Ide_div.appendChild(VisualStudio_btn);
+
+            }
+
             if (checkbox1 == true) {
                 
                 // create a button called github.dev
                 var github_dev = document.createElement("button");
-                github_dev.innerHTML = "Github.Dev";
-                github_dev.className = "dropdown-item btn-link sas";
+                github_dev.innerHTML = "Github.dev";
+                github_dev.className = "dropdown-item btn-link sas ";
                 github_dev.onclick = function() {
                     
                     const newUrl = MainUrl.replace("com", "dev");
@@ -43,15 +65,37 @@ async function init () {
                 Ide_div.appendChild(github_dev);            
     
             }
+
+            if (checkbox2 == true) {
+                var repository_status = document.querySelector("#repository-container-header > div.d-flex.mb-3.px-3.px-md-4.px-lg-5 > div > div > span.Label.Label--secondary.v-align-middle.mr-1");
+                const UserName = GetUsername();
+                if (window.location.href.includes(UserName)) {
+            
+                    if (repository_status.innerText == "Public") {
+                        repository_status.innerText = "Your Public Repository";
+                        repository_status.style.color = "green";
+                        repository_status.style.fontWeight = "bold";
+                    }
+    
+                    if (repository_status.innerText == "Private") {
+                        repository_status.innerText = "Your Private Repository";
+                        repository_status.style.color = "red";
+                        repository_status.style.fontWeight = "bold";
+                    }
+                }
+            }
             
             if (checkbox5 == true) {
                 ReplItBtn = document.createElement("button");
-                ReplItBtn.innerHTML = "Replit";
+                ReplItBtn.innerHTML = "Replit.com";
                 ReplItBtn.className = "dropdown-item btn-link sas";
                 ReplItBtn.onclick = function() {
                     
-                    var newUrl = MainUrl.replace("https://", "");
-                    window.location.href = "https://replit.com/" + newUrl;
+                    const UserName = GetUsername();
+                    const Repository = window.location.href.split("/")[4];
+
+                    var newUrl = "https://repl.it/@" + UserName + "/" + Repository;
+                    window.location.href = newUrl;
                     
                 }
                 Ide_div.appendChild(ReplItBtn);
@@ -59,7 +103,7 @@ async function init () {
             
             if (checkbox6 == true) {
                 var vscode_dev = document.createElement("button");
-                vscode_dev.innerHTML = "VSCode.Dev";
+                vscode_dev.innerHTML = "VSCode.dev";
                 vscode_dev.className = "dropdown-item btn-link sas";
                 vscode_dev.onclick = function() {
     
@@ -68,10 +112,24 @@ async function init () {
                 }
                 Ide_div.appendChild(vscode_dev);
             }
-    
+        
     
         });
         
+        var OpenInVSCodeLocal = document.createElement("button");
+        OpenInVSCodeLocal.innerHTML = "Open in VSCode";
+        OpenInVSCodeLocal.className = "dropdown-item btn-link sas open-in-vscode-local";
+        OpenInVSCodeLocal.onclick = function() {
+            vsLocalLink = "vscode://vscode.git/clone?url=https://github.com/"
+            const UserName = GetUsername();
+            const Repository = window.location.href.split("/")[4];
+            vsLocalLink = vsLocalLink + UserName + "/" + Repository;
+            window.location.href = vsLocalLink;
+        }
+        Ide_div.appendChild(OpenInVSCodeLocal);
+
+
+        // append to the list
         IDE_Details.addEventListener("mousedown", function() {
             IDE_Summary.disabled = true;
         });
@@ -94,8 +152,5 @@ async function init () {
 }
 
 if (window.location.href.includes("github.com")) {
-    
     window.addEventListener("load", init);
-
-
 }
